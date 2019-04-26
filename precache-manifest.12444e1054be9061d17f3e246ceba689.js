e1fd09283a450e7be2bbf91d948e95365e357f0e
JenyZ <jenniferzirk@gmail.com>
2019-04-26 16:53:58 -0400

    Updates

---
+++ precache-manifest.12444e1054be9061d17f3e246ceba689.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "af8998b800ba7d563700ea99ba1dff7c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c5739df7741c24f03dff",
+    "url": "/static/css/main.5ba1d55c.chunk.css"
+  },
+  {
+    "revision": "45124f853ba07aa965c0",
+    "url": "/static/js/2.6efc73d3.chunk.js"
+  },
+  {
+    "revision": "c5739df7741c24f03dff",
+    "url": "/static/js/main.d0c822be.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
