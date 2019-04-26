e92f8ba39445c28de65fb155b706eb619b8c7a00
JenyZ <jenniferzirk@gmail.com>
2019-04-26 17:00:30 -0400

    Updates

---
+++ precache-manifest.3c8098e7c01f318d33729b9b6344b224.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3f2de557ff0a2beb8304ecdcdb41624b",
+    "url": "/clicky-game/index.html"
+  },
+  {
+    "revision": "c5739df7741c24f03dff",
+    "url": "/clicky-game/static/css/main.5ba1d55c.chunk.css"
+  },
+  {
+    "revision": "45124f853ba07aa965c0",
+    "url": "/clicky-game/static/js/2.6efc73d3.chunk.js"
+  },
+  {
+    "revision": "c5739df7741c24f03dff",
+    "url": "/clicky-game/static/js/main.d0c822be.chunk.js"
+  },
+  {
+    "revision": "265538ab0e4606d350e6",
+    "url": "/clicky-game/static/js/runtime~main.27874bd3.js"
+  }
+]);
